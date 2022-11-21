f2501b79521ede5654f14743ce16abc2b9131f0f
athulya2000 <athulyav00@gmail.com>
2022-11-21 23:43:05 +0530

    second commit

--- src/Student.java
+++ src/Student.java
@@ -1,5 +1,29 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Student1{
+    String name;
+    int rollNo;
+    String admissionNo;
+    String college;
+
+    public Student1(String name, int rollNo, String admissionNo, String college) {
+        this.name = name;
+        this.rollNo = rollNo;
+        this.admissionNo = admissionNo;
+        this.college = college;
+    }
+}
 public class Student {
     public static void main(String[] args) {
+        ArrayList<Student1> studentList = new ArrayList<Student1>();
+        while (true) {
+            System.out.println("MENU \n 1.ADD STUDENT \n 2.VIEW ALL STUDENT \n 3. SEARCH STUDENT \n 4.DELETE STUDENT \n 5.EXIT");
+            System.out.println("Choose your option");
+            Scanner s = new Scanner(System.in);
+            int menu = s.nextInt();
 
+
+        }
     }
 }
